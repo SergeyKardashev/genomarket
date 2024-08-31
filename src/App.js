38047023cb1568b1f9f807dbc7d1a496b292fec4
SergeyKardashev <angeldeep@yandex.ru>
2024-08-31 22:12:47 +0700

    feat: boilerplate CRA + Ant

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import { Typography, Divider } from "antd";
+import { HeartTwoTone } from '@ant-design/icons';
+
+const { Title, Paragraph, Text, Link } = Typography;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button icon={<HeartTwoTone twoToneColor="#eb2f96"/>}>Button</Button>
+      <Button><HeartTwoTone/>Button</Button>
+      <Button style={{margin: 50}} size="large" loading>Button</Button>
+      <Button type="primary" block>Button</Button>
+      <Button type="primary" danger>Button</Button>
+      <Button type="primary" disabled>Button</Button>
+
+  <Typography>
+    <Title>Introduction</Title>
+
+    <Paragraph>Body text</Paragraph>
+
+    <Paragraph>
+      After massive project practice, which aims to{' '}
+      <Text strong>
+        uniform the user interface
+      </Text>
+      .
+    </Paragraph>
+
+    <Title level={2}>Guidelines</Title>
+
+    <Paragraph>
+      We supply a series of design principles
+      (<Text code>Sketch</Text> and <Text code>Axure</Text>).
+    </Paragraph>
+
+    <Paragraph>
+      <ul>
+        <li>
+          <Link href="/docs/spec/proximity">Principles</Link>
+        </li>
+        <li>
+          <Link href="/docs/spec/overview">Patterns</Link>
+        </li>
+      </ul>
+    </Paragraph>
+
+    <Paragraph>
+      Press <Text keyboard>Esc</Text> to...
+    </Paragraph>
+
+    <Divider />
+
+    <Paragraph>Paragraph after Divider</Paragraph>
+  </Typography>
     </div>
   );
 }
